9a8322847baa51040853201f6c040545b9d0ecf7
Bohdan Vorobiei <vbs@ukr.net>
2025-12-09 22:13:21 +0200

    task completed

---
+++ src/main/kotlin/mate/academy/Author.kt
@@ -0,0 +1,11 @@
+package mate.academy
+
+class Author (val name: String)
+
+data class Book (
+    val title: String,
+    val authors: List<Author>,
+    val publicationYear: Int,
+    val genre: String,
+    val isbn: String
+)
--- src/main/kotlin/mate/academy/LibraryService.kt
+++ src/main/kotlin/mate/academy/LibraryService.kt
@@ -1,20 +1,45 @@
 package mate.academy
 
+import java.util.concurrent.CopyOnWriteArrayList
+
 class LibraryService {
+    private val books: MutableList<Book> = CopyOnWriteArrayList()
+
     fun addBook(book: Book) {
-        // TODO: implement
+        books.add(book)
     }
 
     fun searchByTitle(title: String): List<Book> {
-        // TODO: implement
+        if (title.isBlank()) return books
+
+        val normalizedTitle = title.trim().lowercase()
+
+        return books.filter { book ->
+            book.title.lowercase().contains(normalizedTitle)
+        }
     }
 
 
     fun searchByAuthor(authorName: String): List<Book> {
-        // TODO: implement
+        if (authorName.isBlank()) return emptyList()
+
+        val normalizedAuthorName = authorName.trim().lowercase()
+
+        return books.filter { book ->
+            book.authors.any { author ->
+                author.name.lowercase().contains(normalizedAuthorName)
+            }
+        }
     }
 
     fun searchByGenre(genre: String): List<Book> {
-        // TODO: implement
+        if (genre.isBlank()) return emptyList()
+
+        val normalizedGenre = genre.trim().lowercase()
+
+        return books.filter { book ->
+            book.genre.lowercase() == normalizedGenre
+        }
     }
 }
+
